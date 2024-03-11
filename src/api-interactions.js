1a4b2124b5c07f1095082635209fc7640015a231
Daniel <roderm99@gmail.com>
2024-03-11 12:04:34 -0400

    created div for desired weather data; created functionality to get location data with input and serch button and display on screen

--- src/api-interactions.js
+++ src/api-interactions.js
@@ -4,7 +4,7 @@
 //     .then((response) => console.log(response));
 
 // eslint-disable-next-line consistent-return
-async function getWeatherDataFor(city) {
+async function getCurrentWeatherDataFor(city) {
 
     try {
         let response = await fetch(`https://api.weatherapi.com/v1/current.json?key=6ec50ea541c74813b6d174204240803&q=${city}`);
@@ -23,7 +23,36 @@ async function getWeatherDataFor(city) {
     }
 }
 
-export { getWeatherDataFor };
+async function getForecastDataFor(city, days) {
+    Number(days);
+    String(city);
+    if (days < 0 || days > 3) {
+
+        return alert("Must be a 3-day forecast, maximum");
+
+    } 
+
+        try {
+            let response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=6ec50ea541c74813b6d174204240803&q=${city}&days=${days}`);
+        
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+        
+            let jsonData = await response.json();
+
+            return jsonData;
+
+        } catch (err) {
+            console.error('there was an error with the fetch operation:', err);
+            throw err;
+        }
+    
+}
+
+
+
+export { getCurrentWeatherDataFor, getForecastDataFor };
     
 
 
--- src/index.js
+++ src/index.js
@@ -1,16 +1,63 @@
-import { getWeatherDataFor } from "./api-interactions";
+import { getCurrentWeatherDataFor, getForecastDataFor } from "./api-interactions";
 // functions I need:
-// function that takes a location and returns the weather data for the location
-//  - function for each piece of weather data I want? or one function that gets everything.
-//  - If one function, then I will need to filter
-
-// function that can display the weather data on the screen
 
 
 
 // practice console-logging weather data
-getWeatherDataFor("Somerville").then((data) => {
+// getCurrentWeatherDataFor("Boston").then((data) => {
+//     console.log("Guess What's Comming!");
+//     console.log(`${data.location.name}, ${data.location.region}, ${data.location.country}`);
+//     console.log(data.current);
+// });
+
+getForecastDataFor("Boston", 3).then((data) => {
     console.log("Guess What's Comming!");
+    console.log(data);
     console.log(`${data.location.name}, ${data.location.region}, ${data.location.country}`);
-    console.log(data.current);
+    console.log(data.forecast);
+    console.log(data.forecast.forecastday[2].day.maxtemp_f)
+    console.log(data.forecast.forecastday[0].date)
 });
+
+const locationInput = document.querySelector('#location-input');
+const searchBtn = document.querySelector('.search-btn');
+const locationTitle = document.querySelector('h2');
+
+const timeSpan = document.querySelector('#current-time');
+const tempSpan = document.querySelector('#current-temp');
+const weatherSpan = document.querySelector('#current-weather');
+const sunsetSpan = document.querySelector('#current-sunset');
+const loadingDiv = document.querySelector('.loading');
+
+
+function resetSpans() {
+    timeSpan.textContent = '';
+    tempSpan.textContent = '';
+    weatherSpan.textContent = '';
+    sunsetSpan.textContent = '';
+}
+
+searchBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    let locationValue = locationInput.value;
+    if (locationValue === '') {
+        return alert('must enter a location');
+    }
+
+    locationTitle.textContent = "";
+    loadingDiv.textContent = 'Loading...';
+
+    getForecastDataFor(locationValue, 3).then((data) => {
+        locationTitle.textContent = `${data.location.name}, ${data.location.region}, ${data.location.country}`;
+        timeSpan.textContent = data.forecast.forecastday[0].date;
+        tempSpan.textContent = data.forecast.forecastday[0].day.maxtemp_f;
+        weatherSpan.textContent = data.forecast.forecastday[0].day.condition.text;
+        sunsetSpan.textContent = data.forecast.forecastday[0].astro.sunset;
+        loadingDiv.textContent = "";
+    }).catch(err => {
+        console.log(err);
+        resetSpans();
+        loadingDiv.textContent = "Couldn't find location";
+    })
+})
+
